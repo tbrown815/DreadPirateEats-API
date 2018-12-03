3e996e7c54427b8f9fb217d0a808e73eb2d301f5
BrotherFatcake <BrotherFatcake@brownscastle.com>
2018-12-02 21:53:51 -0600

    config updates for prod

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 //exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/dreadpirateeats';
-//exports.DATABASE_URL = process.env.DATABASE_URL;
-exports.DATABASE_URL = 'mongodb://localhost/dreadpirateeats';
+exports.DATABASE_URL = process.env.DATABASE_URL;
+//exports.DATABASE_URL = 'mongodb://localhost/dreadpirateeats';
 
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-dreadpirateeats';
@@ -17,4 +17,6 @@ exports.JWT_EXPIRE = process.env.JWT_EXPIRE || '5d';
 
 //module.exports = {CLIENT_ORIGIN: '*'};
 
-exports.CLIENT_ORIGIN = 'http://localhost:3000'
+//exports.CLIENT_ORIGIN = 'http://localhost:3000'
+
+exports.CLIENT_ORIGIN = 'https://shielded-eyrie-90605.herokuapp.com/'
