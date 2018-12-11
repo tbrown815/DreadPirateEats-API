c9c71cd21a05ea5459c275d52705be0daf6b3f00
BrotherFatcake <BrotherFatcake@brownscastle.com>
2018-12-10 18:43:51 -0600

    config updates

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ exports.DATABASE_URL = process.env.DATABASE_URL;
 
 //exports.DATABASE_URL = 'mongodb://localhost/dreadpirateeats';
 
-
 //exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-dreadpirateeats';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 
 
 exports.PORT = process.env.PORT || 8080;
