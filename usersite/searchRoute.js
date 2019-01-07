2a8ad1bbf632804aee1d7b68e0d91b7009b6ea37
BrotherFatcake <BrotherFatcake@brownscastle.com>
2019-01-06 21:37:09 -0600

    file cleanup

--- usersite/searchRoute.js
+++ usersite/searchRoute.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {YELP_TOKEN} = require('../config');
+const { YELP_TOKEN } = require('../config');
 
 const router = express.Router();
 const passport = require('passport');
@@ -12,66 +12,66 @@ const jwtAuth = passport.authenticate('jwt', { session: false });
 const request = require('request-promise');
 
 
-/*GENERAL RESTURANT SEARCH CALL AND FUNCTION*/ 
+/*GENERAL RESTURANT SEARCH CALL AND FUNCTION*/
 let yelpSearchCall = {
     token: YELP_TOKEN,
     location: null,
     term: null,
     categories: '&categories=restaurants',
     sort_by: null,
-    
-    searchResturants: function() {
+
+    searchResturants: function () {
         return request({
             method: 'GET',
             url: 'https://api.yelp.com/v3/businesses/search?' + yelpSearchCall.location + yelpSearchCall.term + yelpSearchCall.categories + yelpSearchCall.sort_by,
             json: true,
-            headers: 
-                {
+            headers:
+            {
                 Authorization: YELP_TOKEN
-                }
-         })
+            }
+        })
     }
 }
 
 function yelpSearchApi(params) {
-    console.log ('params: ', params)
+
     yelpSearchCall.location = params.location
     yelpSearchCall.term = params.term
     yelpSearchCall.sort_by = params.sort_by
 
     return yelpSearchCall.searchResturants()
-        
+
 }
-/*END - GENERAL RESTURANT SEARCH CALL AND FUNCTION*/ 
+/*END - GENERAL RESTURANT SEARCH CALL AND FUNCTION*/
 
 
 
-/*SPECIFIC RESTURANT DETAIL CALL AND FUNCTION*/ 
+/*SPECIFIC RESTURANT DETAIL CALL AND FUNCTION*/
 let yelpDetailCall = {
     token: YELP_TOKEN,
     favYelpId: null,
-    
-    searchResturants: function() {
+
+    searchResturants: function () {
         return request({
             method: 'GET',
             url: 'https://api.yelp.com/v3/businesses/' + yelpDetailCall.favYelpId,
             json: true,
-            headers: 
-                {
+            headers:
+            {
                 Authorization: YELP_TOKEN
-                }
-         })
+            }
+        })
     }
 }
 
 function yelpDetailApi(params) {
-    console.log ('params: ', params)
+
     yelpDetailCall.favYelpId = params.favYelpId
 
     return yelpDetailCall.searchResturants()
-        
+
 }
-/*END - SPECIFIC RESTURANT DETAIL CALL AND FUNCTION*/ 
+/*END - SPECIFIC RESTURANT DETAIL CALL AND FUNCTION*/
 
 
 /*ENDPOINT FOR GENERAL SEARCH USING NAME AND ZIP*/
@@ -95,33 +95,29 @@ router.post('/search', jsonParser, (req, res) => {
     let term = req.body.restaurantName
     let sort_by = req.body.publicSort
 
-    console.log('location1: ', location, 'term: ',term, 'sort_by: ', sort_by)
+    yelpSearchApi({ location: `location=${location}`, term: `&term=${term}`, sort_by: `&sort_by=${sort_by}` })
 
-    yelpSearchApi({location: `location=${location}`, term: `&term=${term}`, sort_by: `&sort_by=${sort_by}`})
-    
-   .then(result => {
-           console.log('GENERAL SEARCH USING NAME AND ZIP')
-           return res.json(result)
-       }
-   ) 
+        .then(result => {
+            return res.json(result)
+        }
+        )
 
 })
 
 
-/*ENDPOINT FOR SPECIFIC RESTURANT DETAIL SEARCH*/ 
+/*ENDPOINT FOR SPECIFIC RESTURANT DETAIL SEARCH*/
 router.post('/search/:id', jsonParser, (req, res) => {
 
     let favYelpId = req.body.favYelpId
 
-    console.log('favYelpId: ', favYelpId)
 
-    yelpDetailApi({favYelpId: `${favYelpId}`})
-    
-   .then(result => {
-           console.log('SPECIFIC RESTURANT DETAIL SEARCH')
-           return res.json(result)
-       }
-   ) 
+    yelpDetailApi({ favYelpId: `${favYelpId}` })
+
+        .then(result => {
+
+            return res.json(result)
+        }
+        )
 
 })
 
